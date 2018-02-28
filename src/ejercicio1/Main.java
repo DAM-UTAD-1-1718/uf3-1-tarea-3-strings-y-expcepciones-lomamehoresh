a79aad23c5bf5bd95afc6542093d6ddd6052fc16
Sergio <Sergio@PortatilClase>
2018-02-28 10:32:41 +0100

    Ejercicio 1 Terminado

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -1,10 +1,32 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Main {
-
+	Scanner sc = new Scanner(System.in);
+	String chain = "";
+	int resultado = 0;
+	
+	public void Escaner() {
+		System.out.println("Introduce una cadena");
+		chain = sc.nextLine();
+		char caracter = 0;
+		int sumando = 0;
+		for (int i = 0; i < chain.length(); i++) {
+            // Determines if the specified character is a digit
+            if (Character.isDigit(chain.charAt(i))) {
+            	caracter = chain.charAt(i);
+            	sumando = Character.getNumericValue(caracter);
+                resultado = resultado + sumando;
+            } 
+        }
+		System.out.println(resultado);	
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Main res = new Main();
+		res.Escaner();
+		
 	}
 
 }
